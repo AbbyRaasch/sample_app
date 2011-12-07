0d4864126973b3d25f64f55f89b1320b961ad5fc
Abby Raasch <abbyjo22@gmail.com>
2011-12-06 19:57:47 -0600

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,13 @@
 module SessionsHelper
-	def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    self.current_user = user
+  
+  def deny_access
+  	 store_location
+  	 redirect_to signin_path, :notice => "Please sign in to access this page."
+  end  
+  
+  def redirect_back_or(default)
+  	 redirect_to(session[:return_to] || default)
+  	 clear_return_to
   end
   
   def current_user=(user)
@@ -21,6 +27,14 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def sign_in(user)
+    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    self.current_user = user
+  end
   private
 
     def user_from_remember_token
@@ -29,5 +43,13 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+    
+    def store_location
+    	session[:return_to] = request.fullpath
+    end
+    
+    def clear_return_to
+    	session[:return_to] = nil
     end
 end
